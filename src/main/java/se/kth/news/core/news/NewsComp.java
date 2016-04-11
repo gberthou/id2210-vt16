b9384db913448ed08f866fa6dfb6f370cca5268c
Alex Ormenisan <ormenisan.adrian@gmail.com>
2016-04-11 14:06:26 +0200

    ping pong msg example

--- src/main/java/se/kth/news/core/news/NewsComp.java
+++ src/main/java/se/kth/news/core/news/NewsComp.java
@@ -17,17 +17,23 @@
  */
 package se.kth.news.core.news;
 
+import java.util.Iterator;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.kth.news.core.leader.LeaderSelectPort;
 import se.kth.news.core.leader.LeaderUpdate;
 import se.kth.news.core.news.util.NewsView;
+import se.kth.news.play.Ping;
+import se.kth.news.play.Pong;
+import se.sics.kompics.ClassMatchedHandler;
 import se.sics.kompics.ComponentDefinition;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Negative;
 import se.sics.kompics.Positive;
 import se.sics.kompics.Start;
 import se.sics.kompics.network.Network;
+import se.sics.kompics.network.Transport;
 import se.sics.kompics.timer.Timer;
 import se.sics.ktoolbox.croupier.CroupierPort;
 import se.sics.ktoolbox.croupier.event.CroupierSample;
@@ -35,6 +41,10 @@ import se.sics.ktoolbox.gradient.GradientPort;
 import se.sics.ktoolbox.gradient.event.TGradientSample;
 import se.sics.ktoolbox.util.identifiable.Identifier;
 import se.sics.ktoolbox.util.network.KAddress;
+import se.sics.ktoolbox.util.network.KContentMsg;
+import se.sics.ktoolbox.util.network.KHeader;
+import se.sics.ktoolbox.util.network.basic.BasicContentMsg;
+import se.sics.ktoolbox.util.network.basic.BasicHeader;
 import se.sics.ktoolbox.util.overlays.view.OverlayViewUpdate;
 import se.sics.ktoolbox.util.overlays.view.OverlayViewUpdatePort;
 
@@ -42,6 +52,7 @@ import se.sics.ktoolbox.util.overlays.view.OverlayViewUpdatePort;
  * @author Alex Ormenisan <aaor@kth.se>
  */
 public class NewsComp extends ComponentDefinition {
+
     private static final Logger LOG = LoggerFactory.getLogger(NewsComp.class);
     private String logPrefix = " ";
 
@@ -62,13 +73,15 @@ public class NewsComp extends ComponentDefinition {
         selfAdr = init.selfAdr;
         logPrefix = "<nid:" + selfAdr.getId() + ">";
         LOG.info("{}initiating...", logPrefix);
-        
+
         gradientOId = init.gradientOId;
 
         subscribe(handleStart, control);
         subscribe(handleCroupierSample, croupierPort);
         subscribe(handleGradientSample, gradientPort);
         subscribe(handleLeader, leaderPort);
+        subscribe(handlePing, networkPort);
+        subscribe(handlePong, networkPort);
     }
 
     Handler handleStart = new Handler<Start>() {
@@ -85,23 +98,50 @@ public class NewsComp extends ComponentDefinition {
         trigger(new OverlayViewUpdate.Indication<>(gradientOId, false, localNewsView.copy()), viewUpdatePort);
     }
 
-    Handler handleCroupierSample = new Handler<CroupierSample>() {
+    Handler handleCroupierSample = new Handler<CroupierSample<NewsView>>() {
         @Override
-        public void handle(CroupierSample sample) {
+        public void handle(CroupierSample<NewsView> castSample) {
+            if (castSample.publicSample.isEmpty()) {
+                return;
+            }
+            Iterator<Identifier> it = castSample.publicSample.keySet().iterator();
+            KAddress partner = castSample.publicSample.get(it.next()).getSource();
+            KHeader header = new BasicHeader(selfAdr, partner, Transport.UDP);
+            KContentMsg msg = new BasicContentMsg(header, new Ping());
+            trigger(msg, networkPort);
         }
     };
-            
+
     Handler handleGradientSample = new Handler<TGradientSample>() {
         @Override
         public void handle(TGradientSample sample) {
         }
     };
-    
+
     Handler handleLeader = new Handler<LeaderUpdate>() {
         @Override
         public void handle(LeaderUpdate event) {
         }
     };
+
+    ClassMatchedHandler handlePing
+            = new ClassMatchedHandler<Ping, KContentMsg<?, ?, Ping>>() {
+
+                @Override
+                public void handle(Ping content, KContentMsg<?, ?, Ping> container) {
+                    LOG.info("{}received ping from:{}", logPrefix, container.getHeader().getSource());
+                    trigger(container.answer(new Pong()), networkPort);
+                }
+            };
+
+    ClassMatchedHandler handlePong
+            = new ClassMatchedHandler<Pong, KContentMsg<?, KHeader<?>, Pong>>() {
+
+                @Override
+                public void handle(Pong content, KContentMsg<?, KHeader<?>, Pong> container) {
+                    LOG.info("{}received pong from:{}", logPrefix, container.getHeader().getSource());
+                }
+            };
 
     public static class Init extends se.sics.kompics.Init<NewsComp> {
 
